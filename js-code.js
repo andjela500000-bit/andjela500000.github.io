d87baaff37ec6a60b4426f05fef52c4f8ef5c839
Angela Zaric <andjela500000@gmail.com>
2026-06-15 15:15:28 +0200

    An update related to project descriptions

--- js-code.js
+++ js-code.js
@@ -4,35 +4,35 @@ const projectsData = {
     "fashion-blog": {
         title: "Fashion Blog Design",
         time: "1 day",
-        description: "A simple design for a fashion blog",
+        description: "A simple design for a fashion blog, with the newest three articles shown on the right side while the main article is shown on the left.",
         skills: ["HTML", "CSS"],
         images: ["images for portfolio/fashion blog main.JPG", "images for portfolio/fashion blog article.JPG", "images for portfolio/fashion blog footer.JPG"]
     },
     "travel-site": {
         title: "Travel Site",
         time: "2 days",
-        description: "Simple site made for a travel agency",
+        description: "Simple website made for a travel agency, with 4 distinct pages related to different topics of the website.",
         skills: ["HTML", "CSS"],
         images: ["images for portfolio/nm main page.JPG", "images for portfolio/nm history.JPG", "images for portfolio/nm prices.JPG", "images for portfolio/nm footer.JPG"]
     },
     "bakery-app": {
         title: "Pastry Shop Website Mockup",
         time: "2 days",
-        description: "Website mockup for mobile phones for showcasing all the products of a pastry shop",
+        description: "Website mockup for mobile phones, for showcasing all the products of a pastry shop and their ingredients as well as the ability to learn the history and get in contact with the pastry shop.",
         skills: ["Figma"],
         images: ["images for portfolio/main page.jpg", "images for portfolio/more information.jpg", "images for portfolio/contact.jpg", "images for portfolio/products.jpg", "images for portfolio/products filter.jpg"]
     },
     "weather-tracker": {
         title: "Weather Tracker App Mockup",
         time: "2 days",
-        description: "App mockup for a weather app, primarily made for mobile phones",
+        description: "App mockup for a weather app, primarily made for mobile phones with a lot of different looks for different weather conditions.",
         skills: ["Figma"],
         images: ["images for portfolio/sunny.jpg", "images for portfolio/mostly sunny.jpg", "images for portfolio/partly cloudy.jpg", "images for portfolio/cloudy.jpg", "images for portfolio/light rain.jpg", "images for portfolio/rain.jpg", "images for portfolio/thunderstorm.jpg", "images for portfolio/light snow.jpg", "images for portfolio/snow.jpg"]
     },
     "food-for-thought": {
         title: "Food For Thought",
         time: "1 day",
-        description: "Informative website about fun and interesting facts of all kinds",
+        description: "Informative website about fun and interesting facts of all kinds, with the newest article shown on the left and the three following articles shown on the right, as well as the ability to comment on the bottom of each open article.",
         url: "https://live-food-for-thoughts.pantheonsite.io/",
         skills: ["WordPress"],
         images: ["images for portfolio/wp main.JPG", "images for portfolio/wp article.JPG", "images for portfolio/wp about.JPG", "images for portfolio/wp contribute.JPG", "images for portfolio/wp footer.JPG"]
@@ -40,7 +40,7 @@ const projectsData = {
     "know-your-parrots": {
         title: "Know Your Parrots",
         time: "2 days",
-        description: "Informative website about parrots, with some articles that are SEO optimized",
+        description: "Informative website about parrots, with some articles that are SEO optimized, and each section on the website is dedicated to different topics related to parrots.",
         url: "https://live-know-your-parrots.pantheonsite.io/",
         skills: ["WordPress"],
         images: ["images for portfolio/wp kyp main.JPG", "images for portfolio/wp kyp content.JPG", "images for portfolio/wp kyp footer.JPG"]
@@ -48,28 +48,28 @@ const projectsData = {
     "adobe-illustrator-1": {
         title: "3D Drawing Of A Tomato",
         time: "1 hour",
-        description: "A 3D drawing of a tomato",
+        description: "A 3D drawing of a tomato.",
         skills: ["Adobe Illustrator"],
         images: ["images for portfolio/adobe i tomato.jpg"]
     },
     "adobe-illustrator-2": {
         title: "Drawing Of A Giraffe",
         time: "3 hour",
-        description: "A cartoony drawing of a giraffe",
+        description: "A cartoony drawing of a giraffe.",
         skills: ["Adobe Illustrator"],
         images: ["images for portfolio/adobe i giraffe.jpg"]
     },
     "adobe-photoshop-1": {
         title: "Magazine",
         time: "2 hours",
-        description: "A front page of a magazine",
+        description: "A front page of a magazine.",
         skills: ["Adobe Photoshop"],
         images: ["images for portfolio/adobe p magazine.jpg"]
     },
     "adobe-photoshop-2": {
         title: "Mockup For A Book Cover",
         time: "2 hour",
-        description: "A mockup for a book cover",
+        description: "A mockup for a book cover.",
         skills: ["Adobe Photoshop"],
         images: ["images for portfolio/adobe p book.jpg"]
     }
